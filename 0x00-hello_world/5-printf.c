cd2133c856b30b8d8d6afcfee483aafbaaf120d6
Nightburnn <Nightburn@gmail.com>
2022-09-08 10:36:25 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+# include <stdio.h>
 /**
- * *main - main block
- * * Return: 0*
+ ** main - entry point
+ *Return: 0*
  *
  */
 int main(void)
-{ printsf("with proper grammar, but the outcome is a piece of art,");
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
