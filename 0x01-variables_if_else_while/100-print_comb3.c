bdb4b88e57c0f10de04535da90c994af824045aa
Nightburnn <Nightburn@gmail.com>
2022-09-09 12:01:56 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-
 /**
-* main - Prints the numbers from 00 to 99
+* main - Prints combination of numbers
 * Return: Always (Success)
 *
 */
+
 int main(void)
 {
 int c, i;
-c = i = '0';
 for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
 {
-putchar(c);
-putchar(i);
-if ((c != '9') || (c == '9' && i != '9'))
+if (c < i)
+{										putchar(c);
+{										putchar(i);
+if (c != '8' || (c == '8' && i != '9'))
 {
 putchar(',');
-
 putchar(' ');
+}
 }
 }
 }
