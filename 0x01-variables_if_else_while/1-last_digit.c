6ca614f54527139e34208906c301364d26409a76
Nightburnn <Nightburn@gmail.com>
2022-09-09 08:42:29 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	if (lastd > 5)
 	{
-		printf("Last digit of %d is %d and greater than 5\n", n, lastd);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
 	{
