1f0947c99b04d1c26c0820d4db2d307ab4b39f74
Nightburnn <Nightburn@gmail.com>
2022-09-13 08:30:29 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* main - prints the alphabet, in lowercase, followed by a new line.
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+
+{
+
+void print_alphabet(void)
+
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n')
+}
+
+}
