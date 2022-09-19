64fb9336140dae308beb1b12c3f26b26be593177
Nightburnn <nightburn2003@gmail.com>
2022-09-19 06:33:00 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *rev_string - point a string in reverse
+ *@s: a pointer given by the main function
+ *Return: 0
+ *
+ */
+
+void rev_string(char *s)
+{
+	int counter = 0;
+	int i = 0;
+
+	char swap;
+
+	while (s[counter] != '\0')
+		counter++;
+	counter -= 1;
+
+	while (counter > 1)
+	{
+		swap = s[i];
+		s[i] = s[counter];
+		s[counter] = swap;
+		counter--;
+		i++;
+	}
+}
