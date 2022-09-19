ed3b6a6c4f1c7a144fe04fec17aa642a731c739f
Nightburnn <nightburn2003@gmail.com>
2022-09-19 04:04:42 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - swap the value of two intgers
+ *@a: first integer
+ *@b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int Night;
+
+	Night = *a;
+	*a = *b;
+	*b = Night;
+}
