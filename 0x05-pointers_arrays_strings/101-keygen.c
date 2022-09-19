d99b9eb5873b6b319c572f1a0b4bb6c0dff9bcab
Nightburnn <nightburn2003@gmail.com>
2022-09-19 09:15:56 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+/**
+ *  main - makes something that cracks 101crackme
+ *  Description: cracking the hacker code
+ *  Return: returns 0 if no error
+ */
+
+int main(void)
+{
+	srand((unsigned int)(time(NULL)));
+	int i;
+	char pass[12];
+
+
+	for (i = 0; i < 12; i++)
+	{
+		pass[i] = 33 + rand() % 94;
+	}
+	pass[i] = '\0';
+	printf("%s\n", pass);
+}
