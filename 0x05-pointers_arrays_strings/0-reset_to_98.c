af8c0447b83d1c3b69568ab7f420376eaea58256
Nightburnn <nightburn2003@gmail.com>
2022-09-19 03:25:19 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - update the int value to 98
+ *@n: pointer given by the main function
+ *Description: change the value of the variable n to 98
+ *
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
