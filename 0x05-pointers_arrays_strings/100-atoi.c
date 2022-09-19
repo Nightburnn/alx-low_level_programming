3541ad18dbcb90830333d2d6ed0bef27260546b7
Nightburnn <nightburn2003@gmail.com>
2022-09-19 09:05:01 -0700

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string to use
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int counter = 0;
+	int i = 0;
+	unsigned int result = 0;
+	int sign = 0;
+
+	while (s[counter] != '\0')
+	{
+		if (s[counter] == '-')
+			sign++;
+		if (s[counter] >= 48 && s[counter] <= 57)
+		{
+			i++;
+			result = result * 10 + s[counter] - '0';
+		}
+		if (i > 0 && (s[counter] < 48 || s[counter] > 57))
+			break;
+		counter++;
+	}
+	if (i == 0)
+		return (0);
+	if (sign % 2 == 0)
+		return (result);
+	return (result * -1);
+}
