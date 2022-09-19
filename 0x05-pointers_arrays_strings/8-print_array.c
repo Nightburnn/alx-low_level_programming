44c27039b29911db6e5bb1dba8bd1de0ffbca998
Nightburnn <nightburn2003@gmail.com>
2022-09-19 08:10:04 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - print an array from main
+ * @a: a pointer given by the main function
+ * @n: an int given by the main function
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
+}
