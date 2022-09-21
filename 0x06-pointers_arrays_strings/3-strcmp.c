9ab5986ce15cca6d51e64f3afe5f7dd4b0432eaf
Nightburnn <nightburn2003@gmail.com>
2022-09-21 10:08:25 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,8 +14,8 @@ int _strcmp(char *s1, char *s2)
 
 	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
 	{
-		str_one;
-		str_two;
+		str_one++;
+		str_two++;
 	}
 	return (*str_one - *str_two);
 }
