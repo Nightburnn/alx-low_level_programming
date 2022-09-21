1d9efcdc29e7bf4e1c62b64863778eb06469f1c4
Nightburnn <nightburn2003@gmail.com>
2022-09-21 06:33:59 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string by number of character
+ * @dest: a poiner to a character
+ * @src: a pointer to a character
+ * @n: value
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
