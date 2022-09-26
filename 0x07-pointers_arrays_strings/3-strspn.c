4c2e8a743b8e8788693115f13105e888a8e5836c
Nightburnn <nightburn2003@gmail.com>
2022-09-26 06:08:02 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,49 @@
 #include "main.h"
 
 /**
- *_strspn - this function returns the length of the intial substring
- *@s: This is the main C string to be scanned
- *@accept: string containing the list of characters to match in s
- *return: ash
+ * _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ * Return: return count
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int night;
-	int burn;
-	int ash = 0; /*add three variable to int*/
 
-	for (night = 0; s[night] != 0; night++)
+	int i, j;
+
+	int night = 0;
+
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+
+	while (str1[i] != '\0')
 	{
-		if (ash != night)
-			break;
+		j = 0;
 
 
-		for (burn = 0; s[burn] != 0; burn++)
+		while (str2[j] != '\0')
 		{
-			if (s[night] == accept[j])
+			if (str2[j] == str1[i])
 			{
-			ash++;
-			break;
+				night++;
+				break;
+
+			}
+			j++;
 		}
+		if (s[i] != accept[j])
+
+		{
+			break;
+
+		}
+		i++;
 	}
-	}
-	return (ash);
+	return (night); /*return the value of night*/
+
 }
