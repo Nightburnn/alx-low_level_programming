500352fbbef639eb51f1bc4701a4d0b7c27d2d06
Nightburnn <nightburn2003@gmail.com>
2022-09-28 08:35:56 -0700

    Task 1. function that prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - print a string in reverse
+ *@s: a string given by main
+ *Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+
+		return;
+
+	 _print_rev_recursion(s + 1);
+
+	 _putchar(*s);
+}
+
