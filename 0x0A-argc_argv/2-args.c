372fbd7c9327f49fe491546816e6b4b9644dc848
Nightburnn <nightburn2003@gmail.com>
2022-09-30 09:00:06 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: the vector that hold the argument
+ * Return: 0, it's void
+ */
+
+int main(int argc, char **argv)
+{
+	int Night = 0;
+
+	while (Night++ < argc)
+
+		printf("%s\n", *argv++)
+
+			return (0);
+}
