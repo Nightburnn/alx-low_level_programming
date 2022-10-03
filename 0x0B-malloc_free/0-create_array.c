ae53af31cd34aa3656f429b53caf8c8c606adc3f
Nightburnn <nightburn2003@gmail.com>
2022-10-03 03:10:24 -0700

    Task 0.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific
+ * @size: the size the main function gave
+ * @c: the character we are given
+ * Return: pointer else return null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int night = 0;
+
+	if (size == 0)
+		return (NULL);
+	ptr = malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	while (night < size)
+	{
+		ptr[night] = c;
+		night++;
+	}
+	return (ptr);
+}
