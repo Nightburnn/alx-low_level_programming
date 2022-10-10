5ca9af76af4b7a94bb25833f863232e372df114f
Nightburnn <nightburn2003@gmail.com>
2022-10-10 08:23:47 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,26 +30,15 @@ char *_copy(char *src)
 
 	if (ptr == NULL)
 	{
-
 		return (NULL);
-
 	}
-
 	for (i = 0; src[i] != '\0'; i++)
-
 	{
-
 		ptr[i] = src[i];
-
 	}
-
-
 	ptr[i] = '\0';
-
 	return (ptr);
-
 }
-
 
 /**
  * new_dog - Create a new dog variable
@@ -58,71 +47,35 @@ char *_copy(char *src)
  * @owner: Owner of the dog
  * Return: Pointer to new dog variable
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
-
 {
-
 	dog_t *logan;
-
 	char *new_name, *new_owner;
 
-
-
 	if (name == NULL || owner == NULL)
-
 	{
-
 		return (NULL);
-
 	}
-
-
-
 	logan = malloc(sizeof(dog_t));
-
 	if (logan == NULL)
-
 	{
-
 		return (NULL);
-
 	}
-
-
-
 	new_name = _copy(name);
-
 	if (new_name == NULL)
-
 	{
-
 		free(logan);
-
 		return (NULL);
-
 	}
-
 	(*logan).name = new_name;
-
 	(*logan).age = age;
-
 	new_owner = _copy(owner);
-
 	if (new_owner == NULL)
-
 	{
-
 		free((*logan).name);
-
 		free(logan);
-
 		return (NULL);
-
 	}
-
 	(*logan).owner = new_owner;
-
 	return (logan);
-
 }
