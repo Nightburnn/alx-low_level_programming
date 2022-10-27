4daa97082013e23fb5bdf8e2adfdee20eb452378
Nightburnn <nightburn2003@gmail.com>
2022-10-27 06:38:48 -0700

    Fixing error in 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,39 +1,33 @@
 #include "main.h"
 
 /**
-* binary_to_uint - binary to unsigned int
-* @b: the binary string we are given
-* Description: does stuff via binary and bit manipulation
-* Return: 0 is error or the converted number
-*/
-
+ * binary_to_uint- convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
-	unsigned int sendback = 1;
-	unsigned int store = 0;
-	int count = 0;
-	int j = 0;
 
-	if (!b)
-		return (0);
+	int night = 0, j;
+	unsigned int number = 0, non_binary = 0;
 
-	while (b[i])
+	if (b == NULL)
+		return (non_binary);
+
+	while (b[night] != '\0')
+		night++;
+	night -= 1;
+	j = 0;
+	while (b[j])
 	{
-		if (b[i] < 48 || b[i] > 49)
-			return (0);
-		i++;
-	}
+		if ((b[j] != '0') && (b[j] != '1'))/*&& logical AND operator*/
+			return (non_binary);
 
-	for (count = 0; i >= 0; i--, count++)
-	{
-		sendback = 1;
-		if (b[i] == '1')
-		{
-			for (j = 0; j < count - 1; j++)
-				sendback *= 2;
-			store += sendback;
-		}
+		if (b[j] == '1')
+			number += (1 * (1 << night));/*<< bitwise left shift operator*/
+		j++;
+
+		night--;
 	}
-	return (store);
+	return (number);
 }
